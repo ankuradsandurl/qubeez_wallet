5d2657f1303d33fde4fe34dbdf0ce23988bf8d41
Gaurav_pandey <gaurav.iec12@gmail.com>
2020-10-07 10:58:46 +0530

    initial commit

---
+++ flutter_projects_ankur/qubeez/android/app/src/main/kotlin/com/qubeez/qubeez/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.qubeez.qubeez
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_projects_ankur/qubeez/lib/welcome.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class WelcomeQubeez extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+      return MaterialApp(
+
+      );
+  }
+}
